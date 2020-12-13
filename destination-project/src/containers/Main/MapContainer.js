f2177393254c0b5d5a3f397fc0599b3cc954a575
minjikim <alswl1500@naver.com>
2020-12-13 17:12:40 +0900

    feat map card position

--- destination-project/src/containers/Main/MapContainer.js
+++ destination-project/src/containers/Main/MapContainer.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import Map from '../../components/Map'
-import { makeStyles } from '@material-ui/core/styles';
 import PrimaryCard from '../../ui/PrimaryCard'
 import { Container } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-const useStyles = makeStyles({
-  root: {
-    display:'flex',
-  },
-});
 
 const MapContainer =() =>{
-  const classes = useStyles();
   return(
     <Container>
-    <Grid container>
-      <Grid item md={3}>
-        <PrimaryCard />
+      <Grid container>
+        <Grid item md={3}xs={12} style={{display:'flex'}}>
+          <div style={{display:'flex',alignItems:'center'}}>
+            <PrimaryCard />
+          </div>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Map/>
+        </Grid>
+        <Grid item md={3}xs={12} style={{display:'flex'}}>
+          <div style={{display:'flex',flexWrap:'wrap',alignContent:'space-between'}}>
+            <PrimaryCard />
+            <PrimaryCard />
+          </div>
+        </Grid>
+        
       </Grid>
-      <Grid item md={6}>
-        <Map/>
-      </Grid>
-      <Grid item md={3}>
-        <PrimaryCard />
-      </Grid>
-      
-    </Grid>
     </Container>
 
 
