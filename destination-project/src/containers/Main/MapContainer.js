58d5a3c877485d4c098dc0397336560ee364104d
minjikim <alswl1500@naver.com>
2020-12-13 19:12:49 +0900

    test mobild map card size

--- destination-project/src/containers/Main/MapContainer.js
+++ destination-project/src/containers/Main/MapContainer.js
@@ -28,23 +28,24 @@ const MapContainer =() =>{
       </Container>
     </Box>
     <Box display={{xs: 'none', sm:'block'}}>
-      <Container>
-        <Grid container>
-          <Grid item md={3}xs={12} style={{display:'flex'}}>
-            <div style={{display:'flex',alignItems:'center'}}>
-              <PrimaryCard />
-            </div>
+    <Container>
+        <Grid container style={{display:'flex',position:'relative'}}>
+          <Grid item md={3} xs={12}>
           </Grid>
           <Grid item md={6} xs={12}>
             <Map/>
           </Grid>
-          <Grid item md={3}xs={12} style={{display:'flex'}}>
-            <div style={{display:'flex',flexWrap:'wrap',alignContent:'space-between'}}>
-              <PrimaryCard />
-              <PrimaryCard />
-            </div>
+          <Grid item md={3} xs={12}>
           </Grid>
-          
+          <div style={{position:'absolute' ,top:100}}>
+            <PrimaryCard/>
+          </div>
+          <div style={{position:'absolute' ,top:240, right:100}}>
+            <PrimaryCard/>
+          </div>
+          <div style={{position:'absolute' ,bottom:90, right:270}}>
+            <PrimaryCard/>
+          </div>
         </Grid>
       </Container>
     </Box>
