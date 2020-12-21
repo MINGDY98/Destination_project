11b0162cee88c3a196d5b1c09247fc07c94121ba
hanseungwoo <seungwoo.han@kscy.kr>
2020-12-21 13:36:20 +0900

    style: footer container 적용

--- destination-project/src/containers/Footer/index.js
+++ destination-project/src/containers/Footer/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import footer from '../../assets/images/sand.jpg'
 import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root:{
-    paddingLeft:100,
-    paddingRight:72,
     display:'flex',
     position:'relative'
   },
@@ -27,10 +26,10 @@ const Footer = () => {
       }}
     >
       <div className={classes.root}>
-        <div style={{paddingTop: 100, paddingBottom: 100}}>
+        <Container style={{paddingTop: 100, paddingBottom: 100}}>
           <Typography style={{color:'#281006',fontWeight:600, fontSize:15}} >BEST</Typography>
           <Typography style={{color:'#281006',fontWeight:600, fontSize:15}} >BEST</Typography>
-        </div>
+        </Container>
       </div>
     </div>
   )
